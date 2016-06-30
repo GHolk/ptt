ef48b7cd829ba94997a40b3f28015e173c7cc5ba
gholk <c34031328@mail.ncku.edu.tw>
2016-07-01 00:13:43 +0800

    [new] HorneyDragon family

--- ptt_backup.sh
+++ ptt_backup.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]
 then exit 1
 else curl -b 'over18=1' "$1" |
-		sed -r 's#href="/([^/])#href="//www.ptt.cc/\1#g' >"~/web/ptt/${1##*/}"
+		sed -r 's#href="/([^/])#href="//www.ptt.cc/\1#g' >~/web/ptt/"${1##*/}"
 fi
 
 exit 0
