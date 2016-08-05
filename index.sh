e5d1032b13c9db209300bacd9abcf323c77a8f56
gholk <c34031328@mail.ncku.edu.tw>
2016-08-05 21:58:27 +0800

    [new] sport 體育協會

--- index.sh
+++ index.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/bash
 
-if cd ~/web/ptt
-then : 
-else 
-	echo 'can not cd `~/web/ptt`. ' >&2
-	exit 4
-fi
-
-mv index.html index_old.html
-head -n 15 index_old.html >index.html
+head -n 15 index_old.html
 #sed -i '/^<body>$/,$d ;  $i<body>' index.html
+
 ls -t *.html |\
 	sed '/^index.*html$/d' |\
-	xargs -d "\n" perl genpttmeta.pl >>index.html
+	xargs -d "\n" perl genpttmeta.pl
 
 if [ "$1" == "-p" ]
 then
+
+	exec 1>&6
+
 	if git add . && git commit && git push
 	then exit 0
 	else 
 		echo "can not push to github! " >&2
 		exit 5
 	fi
+
 fi
