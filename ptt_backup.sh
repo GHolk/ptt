f2fd24fade1c0410b8bbb4ca418d8d46b7feb1e6
gholk <c34031328@mail.ncku.edu.tw>
2016-09-30 19:49:22 +0800

    [new] auto push

--- ptt_backup.sh
+++ ptt_backup.sh
@@ -9,28 +9,28 @@
 if cd ~/web/ptt/
 then : 
 else
-	echo 'can not cd `~/web/ptt/`! ' >&2
-	exit 6
+    echo 'can not cd `~/web/ptt/`! ' >&2
+    exit 6
 fi
 
 
 if [ "$1" == "-p" ]
 then
-	push="./index.sh -p >index.html"
-	shift
+    push="./index.sh -p >index.html"
+    shift
 fi
 
 
 if [ -z "$1" ]
 then read url
+else url="$1"
 fi
 
-url="$1"
 
 if [ -z "$url" ]
 then
-	echo "you need input http url! " >&2
-	exit 1
+    echo "you need input http url! " >&2
+    exit 1
 fi
 
 
@@ -38,23 +38,27 @@ file="${url##*/}"
 
 if [ -s "$file" ]
 then
-	echo "file alread exist! please rename existed one. " >&2
-	exit 2
+    echo "file alread exist! please rename existed one. " >&2
+    exit 2
 fi
 
 
 curl -b 'over18=1' "$url" |
-	sed -r 's#href="/([^/])#href="http://www.ptt.cc/\1#g ;
-		s#"//images\.ptt\.cc/v.\.../(.*\.css)"#"\1"#g' >"$file"
+    sed -r '
+        s#="/([^/])#href="http://www.ptt.cc/\1#
+        s#="//images\.ptt\.cc/[^/]+/#="#
+        s#="//ajax\.googleapis\.com/ajax/libs/jquery/[^/]+/#="#
+        s#src="//#src="http://#g
+        ' >"$file"
 
 
 if [ -s "$file" ]
 then
-	echo "http://gholk.github.io/ptt/$file"
-	eval $push
-	exit 
+    echo "http://gholk.github.io/ptt/$file"
+    eval $push
+    exit 
 else
-	echo "file do not created! " >&2
-	exit 3
+    echo "file do not created! " >&2
+    exit 3
 fi
 
