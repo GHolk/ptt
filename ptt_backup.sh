76420d54557b19df98f31a5e07eb94c35dc71336
gholk <c34031328@mail.ncku.edu.tw>
2016-10-20 13:59:52 +0800

    [new] auto push

--- ptt_backup.sh
+++ ptt_backup.sh
@@ -1,17 +1,56 @@
 #!/usr/bin/sh
 
 # error code: 
-# 1: no input url by argument and stdin. 
-# 2: already exit file with same name in `~/web/ptt/` . 
+# 1: invalid url.
+# 2: already exist file with same name in `~/web/ptt/` . 
 # 3: file do not created. 
 # 6: can not cd `~/web/ptt/` . 
 
 error() {
     echo "$1" >&2
-    [ $2 ] && exit $2
+    [ -n "$2" ] && exit $2
 }
 
-cd ~/web/ptt/ || error 'can not cd `~/web/ptt/`! ' 6
+curl_sed() {
+
+    url="$1"
+    file="$(echo $url | sed 's#.*/##')"
+
+    if [ -z "$url" ] || [ -z "$file" ]
+    then
+        error 'invalid url!'
+        return 1
+    fi
+
+    if [ -s "$file" ] 
+    then
+        error "file alread exist! please rename existed one. "
+        return 2
+    fi
+
+    if curl -Ob 'over18=1' "$url"
+    then
+        sed -i -r '
+            s#="/([^/])#href="http://www.ptt.cc/\1#
+            s#="//images\.ptt\.cc/[^/]+/#="#
+            s#="//ajax\.googleapis\.com/ajax/libs/jquery/[^/]+/#="#
+            s#src="//#src="http://#g
+        ' "$file"
+    else
+        error 'invalid url!'
+        return 1
+    fi
+
+    if [ -s "$file" ] 
+    then
+        echo "http://gholk.github.io/ptt/$file"
+    else
+        error "file do not created! "
+        return 3
+    fi
+}
+
+cd ~/web/ptt/ || error 'can not cd `~/web/ptt/`!' 6
 
 if [ "$1" = "-p" ]
 then
@@ -20,32 +59,17 @@ then
 fi
 
 
-if [ -z "$1" ]
-then read url
-else url="$1"
+if [ -n "$1" ]
+then
+    curl_sed "$1"
+else
+    while read url
+    do
+        curl_sed "$url"
+    done
 fi
 
 
-[ -z "$url" ] && error "you need input http url! " 1
-
-
-file="$( echo $url | sed 's#.*/##' )"
-
-[ -s "$file" ] && error "file alread exist! please rename existed one. " 2
-
-
-curl -b 'over18=1' "$url" |
-    sed -r '
-        s#="/([^/])#href="http://www.ptt.cc/\1#
-        s#="//images\.ptt\.cc/[^/]+/#="#
-        s#="//ajax\.googleapis\.com/ajax/libs/jquery/[^/]+/#="#
-        s#src="//#src="http://#g
-    ' >"$file"
-
-
-[ -s "$file" ] || error "file do not created! " 3
-
-echo "http://gholk.github.io/ptt/$file"
 $push
-exit 
+exit
 
